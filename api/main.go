7db36dde2a84a35b0ed3072ae593f1c3fcc5902e
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-19 21:23:20 +0530

    Add GET handler

--- api/main.go
+++ api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
 
 func main() {
-	fmt.Println("hello world")
+	app := fiber.New()
+
+	// for health monitoring of server
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.JSON("hello I am alive...")
+	})
+
+	app.Get("/books", func(c *fiber.Ctx) error {
+		return c.JSON(books)
+	})
+
+	if err := app.Listen(":8080"); err != nil {
+		panic(err)
+	}
 }
